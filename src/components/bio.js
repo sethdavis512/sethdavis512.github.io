ecab80bfecfda5242a217bc0d57d3e618522ece3
Seth Davis <sethdavis@indeed.com>
2019-10-07 22:17:00 -0500

    Replace default content with TWS

--- src/components/bio.js
+++ src/components/bio.js
@@ -12,7 +12,7 @@ import Image from "gatsby-image"
 import { rhythm } from "../utils/typography"
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
+    const data = useStaticQuery(graphql`
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
@@ -26,43 +26,51 @@ const Bio = () => {
           author
           social {
             twitter
+            github
+            codepen
           }
         }
       }
     }
   `)
 
-  const { author, social } = data.site.siteMetadata
-  return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
-      </p>
-    </div>
-  )
+    const { author, social } = data.site.siteMetadata;
+
+    return (
+        <div
+            style={{
+                display: `flex`,
+                marginBottom: rhythm(2.5),
+            }}
+        >
+            <Image
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author}
+                style={{
+                    marginRight: rhythm(1 / 2),
+                    marginBottom: 0,
+                    minWidth: 50,
+                    borderRadius: `100%`,
+                }}
+                imgStyle={{
+                    borderRadius: `50%`,
+                }}
+            />
+            <p>
+                Written by <strong>{author}</strong>.
+                <br />
+                <a href={`https://github.com/${social.github}`}>
+                    GitHub
+                </a>{' '}
+                <a href={`https://codepen.com/${social.codepen}`}>
+                    Codepen
+                </a>{' '}
+                <a href={`https://twitter.com/${social.twitter}`}>
+                    Twitter
+                </a>
+            </p>
+        </div>
+    )
 }
 
 export default Bio
